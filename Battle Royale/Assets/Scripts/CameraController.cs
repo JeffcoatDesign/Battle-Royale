af4d3173f98690a0af611ac0b909d626322556f4
JeffcoatDesign <98057241+JeffcoatDesign@users.noreply.github.com>
2022-09-21 23:15:52 -0700

    Lessons 1-16

---
+++ Battle Royale/Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Look Sensitivity")]
+    public float sensX;
+    public float sensY;
+
+    [Header("Clamping")]
+    public float minY;
+    public float maxY;
+
+    [Header("Spectator")]
+    public float spectatorMoveSpeed;
+
+    private float rotX;
+    private float rotY;
+
+    private bool isSpectator = false;
+
+    void Start ()
+    {
+        //lock cursor to middle of screen.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        rotX += Input.GetAxis("Mouse X") * sensX;
+        rotY += Input.GetAxis("Mouse Y") * sensY;
+
+        rotY = Mathf.Clamp(rotY, minY, maxY);
+
+        if(isSpectator)
+        {
+            //rotate camera vertically
+            transform.rotation = Quaternion.Euler(-rotY, rotX, 0);
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            float y = 0;
+
+            if (Input.GetKey(KeyCode.E))
+                y = 1;
+            else if (Input.GetKey(KeyCode.Q))
+                y = -1;
+
+            Vector3 dir = transform.right * x + transform.up * y + transform.forward * z;
+            transform.position += dir * spectatorMoveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            //rotate the cam vertically
+            transform.localRotation = Quaternion.Euler(-rotY, 0, 0);
+
+            //rotate the player horizontally
+            transform.parent.rotation = Quaternion.Euler(0, rotX, 0);
+        }
+    }
+}
